6173956ab1d3a83d4453be36eb4b38e2c83ca671
Erick Lemos <ericklemos.dev@gmail.com>
2024-08-30 15:20:32 -0300

    terceiro commit

---
+++ src/exercicios/Ex11.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 30; i++) {
+			if(i < 18) {
+				System.out.println(i +" é menor de idade");
+			}
+			else {
+				System.out.println(i +" é maior de idade");
+			}
+		}
+
+	}
+
+}
